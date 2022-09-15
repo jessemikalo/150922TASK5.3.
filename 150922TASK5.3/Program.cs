28b8adbd814a7f99f7fa48caf411a2e44176c8a9
jesse <jesse@JESSE>
2022-09-15 11:12:22 +0300

    Add project files.

---
+++ 150922TASK5.3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Count the total number of alphabets, digits and special characters: ");
+Console.Write("Input the string");
+string inputTheString = Console.ReadLine();
+
+Console.WriteLine("Number of Alphabets in the string is");
+do
